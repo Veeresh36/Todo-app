98d732306b830d7f57b9660b513505b7176a508f
Veeresh Bashetti <veereshbashetti121@gmail.com>
2025-12-06 21:13:39 +0530

    server updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate,  } from 'react-router-dom'
 import Dataadd from '../src/components/dataadd.jsx'
 import Sidemenu from './components/sideMenu.jsx'
 import Tasklist from '../src/components/taskslist.jsx'
